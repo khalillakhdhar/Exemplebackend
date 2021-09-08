1a85caeb3ba0351df871cba8e489f4b7c9eaf164
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-08 12:43:19 +0100

    Update CandidatController.java

--- src/main/java/com/http/tp/controller/CandidatController.java
+++ src/main/java/com/http/tp/controller/CandidatController.java
@@ -1,5 +1,37 @@
 package com.http.tp.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.http.tp.model.repositories.CandidatRepository;
+import com.http.tp.model.entities.Candidat;
+
+@RestController
+@RequestMapping
 public class CandidatController {
+@Autowired
+CandidatRepository candidatRepository;
+@GetMapping
+public List<Candidat> getAllCandidats()
+{
+	return  candidatRepository.findAll();
+
+}
+@PostMapping
+public Candidat addCandidat(@Valid @RequestBody Candidat candidat)
+{
+	return candidatRepository.save(candidat);
+	
+}
+
+
 
 }
