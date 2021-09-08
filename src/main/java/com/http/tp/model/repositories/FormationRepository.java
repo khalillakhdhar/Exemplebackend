c0edb0a3bf7741dd22d06d64c1332eb3d7cbd537
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-08 10:37:54 +0100

    Create FormationRepository.java

---
+++ src/main/java/com/http/tp/model/repositories/FormationRepository.java
@@ -0,0 +1,9 @@
+package com.http.tp.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.http.tp.model.entities.Formation;
+
+public interface FormationRepository extends CrudRepository<Formation, Long> {
+
+}
