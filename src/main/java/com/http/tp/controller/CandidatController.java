42f60b1513823920654b64d2e5d33a1bf58920ea
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-08 13:42:05 +0100

    Update CandidatController.java

--- src/main/java/com/http/tp/controller/CandidatController.java
+++ src/main/java/com/http/tp/controller/CandidatController.java
@@ -5,17 +5,22 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.http.tp.model.repositories.CandidatRepository;
+import com.http.tp.exception.ResourceNotFoundException;
 import com.http.tp.model.entities.Candidat;
+import com.http.tp.model.entities.Formation;
 
 @RestController
-@RequestMapping
+@RequestMapping("candidat")
 public class CandidatController {
 @Autowired
 CandidatRepository candidatRepository;
@@ -31,7 +36,20 @@ public Candidat addCandidat(@Valid @RequestBody Candidat candidat)
 	return candidatRepository.save(candidat);
 	
 }
+@GetMapping("/{id}")
+public Candidat getDeviceById(@PathVariable(value = "id") Long candidatId)
+{
+return candidatRepository.findById(candidatId).orElseThrow(() -> new ResourceNotFoundException("candidat", "id",candidatId));
+}
+@DeleteMapping("/{id}")
+public ResponseEntity<?> deleteFormation(@PathVariable(value = "id") Long candidatId) {
+    Candidat c = candidatRepository.findById(candidatId)
+            .orElseThrow(() -> new ResourceNotFoundException("admin", "id", candidatId));
 
+    candidatRepository.delete(c);
+
+    return ResponseEntity.ok().build();
+}
 
 
 }
