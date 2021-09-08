d0184bc94f54c952067167243cbc22fa8fc68769
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-08 10:54:17 +0100

    Update Formation.java

--- src/main/java/com/http/tp/model/entities/Formation.java
+++ src/main/java/com/http/tp/model/entities/Formation.java
@@ -2,12 +2,17 @@ package com.http.tp.model.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 import com.sun.istack.NotNull;
 
 @Entity
 public class Formation {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private long id;
 @NotBlank
 private String titre;
